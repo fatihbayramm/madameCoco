d8f231854e0021c6b132459478a5a66fd20b8299
fatih bayram <fatihbayramybs@gmail.com>
2023-10-03 00:30:58 +0300

    gorunum acisindan duzenlemeler yapildi .

--- static/main.js
+++ static/main.js
@@ -1,29 +1,64 @@
 const productListDOM = document.querySelector(".product-list");
 const doubleLayoutDOM = document.querySelector("#doublelayout");
 const defaultLayoutDOM = document.querySelector("#defaultlayout");
-const hexaLayoutDOM = document.querySelector("#hexalayout");    
+const hexaLayoutDOM = document.querySelector("#hexalayout");  
+const productInfoDOM = document.querySelector(".product-info");
+const hexaClass = document.querySelector(".product_list_hexa");
+const productListTopDOM = document.querySelector(".product-list-top");
 
-function doubleLayout () {
+
+function removeClass() {
+    productListDOM.removeAttribute("class");
+}
+
+function doubleLayout() {
     doubleLayoutDOM.addEventListener("click", () => {
-        productListDOM.removeAttribute("class");
+        removeClass();
         productListDOM.classList.add("product_list_double");
 })
 }
 
-function defaultLayout () {
+function defaultLayout() {
     defaultLayoutDOM.addEventListener("click", () => {
-        productListDOM.removeAttribute("class");
+        removeClass();
         productListDOM.classList.add("product-list");
 })
 }
 
-function hexaLayout () {
+function hexaLayout() {
     hexaLayoutDOM.addEventListener("click", () => {
-        productListDOM.removeAttribute("class");
+        removeClass();
         productListDOM.classList.add("product_list_hexa");
+        
 })  
 }
+
+
 
 doubleLayout ();
 defaultLayout ();
 hexaLayout ();
+
+console.log("Fatih Bayram is here !");
+
+
+
+{/* <div class="product_list_hexa">
+<% products.forEach(product => { %>
+    <div class="product_list_div">
+        <div>
+            <a href=/product/<%= product.pk %>">
+                <img src="<%= product.productimage_set[0].image %>" class="product-image"
+                alt="Bu dosyanın uzantısı tarayıcınız tarafından desteklenmiyor.">    
+            </a>
+        </div>
+    </div>
+<% }); %>
+</div> */}
+
+
+
+
+
+
+
