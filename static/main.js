f5f560477327a00f0ca522142a97f856a5d152a9
fatih bayram <fatihbayramybs@gmail.com>
2023-10-22 16:44:04 +0300

    detay sayfasina urunlerin farkli renkli resimleri eklendi.

--- static/main.js
+++ static/main.js
@@ -62,25 +62,28 @@ function productCounter() {
     })
 }
 
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
+function accordionMenu() {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+          this.classList.toggle("active");
+          var panel = this.nextElementSibling;
+          if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+          } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+          } 
+        });
+      }
 }
+
 
 doubleLayout ();
 defaultLayout ();
 hexaLayout ();
 productCounter();
+accordionMenu();
 
 console.log("Fatih Bayram is here !");
 
