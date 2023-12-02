a0013e44b09c8a7943e46457daa340a41ee3d1f4
fatih bayram <fatihbayramybs@gmail.com>
2023-12-02 17:45:16 +0300

    classlara js prefix i eklendi.

--- static/main.js
+++ static/main.js
@@ -1,215 +1,198 @@
 const productListDOM = document.querySelector(".js-product-list");
-// js ile etkilesimde olan element class veya idlerine js prefix i eklenecek.
-// ornek #doublelayout #js-doublelayout ek olarak js prefix li bir class asla 
-// css dosyasinda olmamali.
 function removeClass() {
-    productListDOM.removeAttribute("class");
+  productListDOM.removeAttribute("class");
 }
 
 function doubleLayout() {
-    const doubleLayoutDOM = document.querySelector("#doublelayout");
-    if (!doubleLayoutDOM) {
-        return 
-    }
-    doubleLayoutDOM.addEventListener("click", () => {
-        removeClass();
-        productListDOM.classList.add("product_list_double");
-})
+  const doubleLayoutDOM = document.querySelector("#js-DoubleLayout");
+  if (!doubleLayoutDOM) {
+    return;
+  }
+  doubleLayoutDOM.addEventListener("click", () => {
+    removeClass();
+    productListDOM.classList.add("product_list_double");
+  });
 }
 
 function defaultLayout() {
-    const defaultLayoutDOM = document.querySelector("#defaultlayout");
-    if (!defaultLayoutDOM) {
-        return 
-    }
-    defaultLayoutDOM.addEventListener("click", () => {
-        removeClass();
-        productListDOM.classList.add("js-product-list");
-})
+  const defaultLayoutDOM = document.querySelector("#js-default-layout");
+  if (!defaultLayoutDOM) {
+    return;
+  }
+  defaultLayoutDOM.addEventListener("click", () => {
+    removeClass();
+    productListDOM.classList.add("product-list");
+  });
 }
 
 function hexaLayout() {
-    const hexaLayoutDOM = document.querySelector("#hexalayout"); 
-    if (!hexaLayoutDOM) {
-        return 
-    }
-    hexaLayoutDOM.addEventListener("click", () => {
-        removeClass();
-        productListDOM.classList.add("product_list_hexa");
-})  
+  const hexaLayoutDOM = document.querySelector("#js-hexa-layout");
+  if (!hexaLayoutDOM) {
+    return;
+  }
+  hexaLayoutDOM.addEventListener("click", () => {
+    removeClass();
+    productListDOM.classList.add("product_list_hexa");
+  });
 }
 
 function productCounter() {
-    const deacreaseBtnDOM = document.querySelector("#deacreaseButton");
-    const increaseBtnDOM = document.querySelector("#increaseButton");
-    const quantityDOM = document.querySelector(".quantity");
-    if (!deacreaseBtnDOM) {
-        return
-    } 
-    let number = 1;
+  const deacreaseBtnDOM = document.querySelector(".js-deacrease-button");
+  const increaseBtnDOM = document.querySelector(".js-increase-button");
+  const quantityDOM = document.querySelector(".js-quantity");
+  if (!deacreaseBtnDOM) {
+    return;
+  }
+  let number = 1;
+  quantityDOM.innerHTML = number;
+  deacreaseBtnDOM.addEventListener("click", () => {
+    if (number == 1) {
+      return;
+    }
+    number--;
     quantityDOM.innerHTML = number;
-    deacreaseBtnDOM.addEventListener("click", () => {
-        if (number == 1 ) {
-            return 
-        } 
-        number--;
-        quantityDOM.innerHTML = number;
-    })
-    
-    increaseBtnDOM.addEventListener("click", () => {
-        number++;
-        quantityDOM.innerHTML = number;
-    })
+  });
+
+  increaseBtnDOM.addEventListener("click", () => {
+    number++;
+    quantityDOM.innerHTML = number;
+  });
 }
 
 function accordionMenu() {
-    var acc = document.getElementsByClassName("accordion");
-    var i;
-    for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function() {
-          this.classList.toggle("active");
-          var panel = this.nextElementSibling;
-          if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-          } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-          } 
-        });
+  var acc = document.getElementsByClassName("js-accordion");
+  var i;
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
       }
+    });
+  }
 }
 
 function runFilterBox() {
-    const filterBtnDOM = document.querySelector(".js-filter-button");
-    if(!filterBtnDOM) {
-        return
+  const filterBtnDOM = document.querySelector(".js-filter-button");
+  if (!filterBtnDOM) {
+    return;
+  }
+  const filterDivDOM = document.querySelector(".js-filter-div");
+  filterDivDOM.style.display = "none";
+  let counter = 0;
+  filterBtnDOM.addEventListener("click", () => {
+    counter++;
+    let filterStyle = filterBtnDOM.style;
+    if (counter % 2 == 1) {
+      filterBtnDOM.style.color = "#B94E0C";
+      filterDivDOM.style.display = "flex";
+      filterDivDOM.style.borderTop = "1px solid black";
+    } else {
+      filterBtnDOM.style.color = "#323E48";
+      filterDivDOM.style.display = "none";
     }
-    const filterDivDOM = document.querySelector(".filter-div");
-    filterDivDOM.style.display = "none";
-    let counter = 0;
-    filterBtnDOM.addEventListener("click", () => {
-        counter++;
-        let filterStyle = filterBtnDOM.style;
-        if (counter % 2 == 1) {
-            filterBtnDOM.style.color = "#B94E0C";
-            filterDivDOM.style.display = "flex";
-            filterDivDOM.style.borderTop = "1px solid black";
-        } else {
-            filterBtnDOM.style.color = "#323E48";
-            filterDivDOM.style.display = "none";
-        } 
-    })
+  });
 }
 
 function runOrderFilter(event) {
-    event.stopPropagation();
-    document.getElementById("orderDropdown").classList.toggle("show");
+  event.stopPropagation();
+  document.getElementById("js-orderDropdown").classList.toggle("show");
 }
 
-window.onclick = function(event) {
-    if (!event.target.matches(".orderButton")) {
-      var dropdowns = document.getElementsByClassName("orderDropdownContent");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains("show")) {
-            openDropdown.classList.remove("show");
-        }
+window.onclick = function (event) {
+  if (!event.target.matches(".js-order-button")) {
+    var dropdowns = document.getElementsByClassName(
+      "js-order-dropdown-content"
+    );
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
-    } 
-}
-
-
+    }
+  }
+};
 
 function runColorFilter(event) {
-    event.stopPropagation();
-    document.getElementById("colorDropdown").classList.toggle("show");
+  event.stopPropagation();
+  document.getElementById("js-color-Dropdown").classList.toggle("show");
 }
-  window.onclick = function(event) {
-    if (!event.target.matches(".colorFilterButton")) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains("show")) {
-            openDropdown.classList.remove("show");
-        }
+window.onclick = function (event) {
+  if (!event.target.matches(".js-color-Filter-Button")) {
+    var dropdowns = document.getElementsByClassName("js-dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
-    } 
-}
+    }
+  }
+};
 
 function runMeasureFilter(event) {
-    event.stopPropagation();
-    document.getElementById("measureDropdown").classList.toggle("show");
+  event.stopPropagation();
+  document.getElementById("js-measure-Dropdown").classList.toggle("show");
 }
 
-window.onclick = function(event) {
-    if (!event.target.matches(".measureFilterButton")) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains("show")) {
-            openDropdown.classList.remove("show");
-        }
+window.onclick = function (event) {
+  if (!event.target.matches(".js-measure-Filter-Button")) {
+    var dropdowns = document.getElementsByClassName("js-dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
-    } 
-}
+    }
+  }
+};
 
 function runMaterialFilter(event) {
-    event.stopPropagation();
-    document.getElementById("materialDropdown").classList.toggle("show");
+  event.stopPropagation();
+  document.getElementById("js-material-Dropdown").classList.toggle("show");
 }
 
-window.onclick = function(event) {
-    if (!event.target.matches(".materialFilterButton")) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains("show")) {
-            openDropdown.classList.remove("show");
-        }
+window.onclick = function (event) {
+  if (!event.target.matches(".js-material-Filter-Button")) {
+    var dropdowns = document.getElementsByClassName("js-dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
-    } 
-}
+    }
+  }
+};
 
 function runPriceFilter(event) {
-    event.stopPropagation();
-    document.getElementById("priceDropdown").classList.toggle("show");
+  event.stopPropagation();
+  document.getElementById("js-price-Dropdown").classList.toggle("show");
 }
 
-window.onclick = function(event) {
-    if (!event.target.matches(".priceFilterButton")) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains("show")) {
-            openDropdown.classList.remove("show");
-        }
+window.onclick = function (event) {
+  if (!event.target.matches(".js-price-Filter-Button")) {
+    var dropdowns = document.getElementsByClassName("js-dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
-    } 
-}
+    }
+  }
+};
 
-
-
-
-doubleLayout ();
-defaultLayout ();
-hexaLayout ();
+doubleLayout();
+defaultLayout();
+hexaLayout();
 accordionMenu();
 productCounter();
 runFilterBox();
 
-
-
-
 console.log("Fatih Bayram is here !");
-
-
-
-
-
-
-
-
